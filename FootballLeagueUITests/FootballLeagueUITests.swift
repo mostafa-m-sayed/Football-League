b5179b1b83c591e9af5c8602e531e657de1f71db
Mostafa Sayed <mostafa-m-sayed@hotmail.com>
2020-10-18 23:41:04 +0200

    Basic UI Testing

--- FootballLeagueUITests/FootballLeagueUITests.swift
+++ FootballLeagueUITests/FootballLeagueUITests.swift
@@ -8,35 +8,28 @@
 import XCTest
 
 class FootballLeagueUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    let app = XCUIApplication()
+    
+    func testGoToFavourites() {
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tabBar = XCUIApplication().tabBars["Tab Bar"]
+        tabBar.children(matching: .other).element(boundBy: 1).tap()
+        XCTAssertTrue(app.otherElements["FavouritesVC"].waitForExistence(timeout: 5))
+    }
+    
+    func testFavouriteTeam() {
+        app.launch()
+        let table = XCUIApplication().tables//.cells[0].
+        let cell = table.children(matching: .cell).firstMatch
+        cell.buttons["love"].tap()
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testGoToTeam() {
+        app.launch()
+        let table = XCUIApplication().tables
+        let cell = table.children(matching: .cell).firstMatch
+        cell.tap()
+        XCTAssertTrue(app.otherElements["TeamVC"].waitForExistence(timeout: 5))
     }
 }
